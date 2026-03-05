d67b7dac9fd91b3a0b931ca0a354748ae52898ee
Roku <rokufoxpg06@gmail.com>
2026-03-04 22:37:45 -0500

    Add book scraper script and generated data

---
+++ scraper.py
@@ -0,0 +1,21 @@
+import requests as rq
+import csv
+from bs4 import BeautifulSoup as bf
+
+page1 = "https://books.toscrape.com/"
+respuesta = rq.get(url=page1)
+respuesta.encoding = 'utf-8'
+contenido = respuesta.text
+soup = bf(contenido, 'html.parser')
+libros = soup.find_all('article', class_="product_pod")
+
+with open('libros.csv', mode='w', encoding='utf-8', newline='') as f:
+
+    writer = csv.writer(f)
+    writer.writerow(['Título', 'Precio'])
+
+    for libro in libros:
+        titulo = libro.h3.a['title']
+        precio = libro.find('p', class_='price_color').text
+        writer.writerow([titulo, precio])
+        print(f"Guardando: {titulo}")
